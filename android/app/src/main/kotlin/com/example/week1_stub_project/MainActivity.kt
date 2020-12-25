80d0c4cf43c2c8b76ec7b2db0aa9097a33e444d0
Clayrise <54859935+Clayrisee@users.noreply.github.com>
2020-12-25 20:54:51 +0700

    Stub Project Week-1

---
+++ android/app/src/main/kotlin/com/example/week1_stub_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.week1_stub_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("This is Stub Porject"),
+    );
+  }
+
+// TODO 1: Tambahkan MaterialApp sebagai root aplikasi
+// TODO 2 : Buat file baru bernama splash_page.dart
+// TODO 3 : Tambahkan folder baru bernama assets untuk logo dsc
+// TODO 4 : Update pubspec.ymal
+// TODO 5 : Tambahkan file constant.dart sebagai base UI
+// TODO 6 : Design UI login_page.dart
+// TODO  7 : Tambahkan file home_page.dart
+// TODO 8 : Design home_page.dart
+
+}
